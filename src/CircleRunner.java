81f409f9d1f4e591c16cf9593a2749a981db8895
newman-csa <logann24@nycstudents.net>
2023-09-28 08:18:15 -0400

    Do Now

--- src/CircleRunner.java
+++ src/CircleRunner.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class CircleRunner {
+public class CircleRunner {
+    public static void main(String[] args) {
+        // A. create a Circle object with radius 5.0
+        Circle circle1 = new Circle(5.0);
+
+        // B. locate the method defined in the Circle class and write
+        //    a line of code to call it on your object
+        circle1.printArea();
+
+        // C. BEFORE running this code, PREDICT what gets printed
+        System.out.println("I think the function will print \"My area is: 15.7\"");
+
+        // D. run the code and confirm your prediction! Were you correct?
+        System.out.println("My prediction was wrong because I can't do math.");
+    }
 }
+
