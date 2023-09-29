9bd97fefa36c784d14c3497abcb0a55698305822
newman-csa <logann24@nycstudents.net>
2023-09-29 01:00:13 -0400

    ChatBot Complete

--- src/ChatBotRunner.java
+++ src/ChatBotRunner.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
-public class ChatBotRunner {
-    public static void main(String arg)
+public class ChatBotRunner 
+{
+    public static void main(String[] args)
     {
         Scanner scn = new Scanner(System.in);   
         ChatBot winter = new ChatBot("Winter", 21);
@@ -17,7 +18,7 @@ public class ChatBotRunner {
         int num1 = scn.nextInt();
         int num2 = scn.nextInt();
         int num3 = scn.nextInt();
-        System.out.println("Ok, the sum of those numbers is "winter.addNumbers(num1, num2, num3));
+        System.out.println("Ok, the sum of those numbers is "+ winter.addNumbers(num1, num2, num3));
         System.out.println("That number converted to meters is "+ winter.convertFeetToMeters(winter.addNumbers(num1, num2, num3)));
         System.out.println(winter.goodbye());
 
